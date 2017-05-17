73ee3089a7d814d391df41a87f3cefbd1e07af3c
Alex Moskalyuk <moskalyuk@gmail.com>
2017-05-17 02:18:08 -0700

    Actually not breaking spaces this time around

--- wrap.php
+++ wrap.php
@@ -11,57 +11,87 @@ function wrap ($string, $length) {
     return $string;
   }
 
-  // Pass 1: split on *any* whitespace character
-  $words =  preg_split("/[\s]+/", $string);
+  $words = tokenize($string);
+  $result_arr = array();
+  $current_line = '';
 
-  // Pass 2: break down any words longer than $length
-  $words2 = array();
   foreach ($words as $word) {
-    $word = trim($word);
-    if (strlen($word) < 1) {
-      // this was an empty whitespace character that got trimmed out
-      next;
-    }
-    if (strlen($word) > $length) {
-      // break this long word into an array of its own
-      $chunks = str_split($word, $length);
-      // just append to a larger array
-      $words2 = array_merge($words2, $chunks);
+    if (strlen($current_line . $word) <= $length) {
+      // the next word fits and doesn't overflow
+      $current_line .= $word;
     } else {
-      // just a normal sub-length word
-      $words2[] = $word;
+      // unload the current line, but trim the extraneous space in the end
+      if (rtrim($current_line)) {
+        $result_arr []= rtrim($current_line);
+      }
+
+      if (strlen($word) < $length) {
+        $current_line = trim($word); // and move on to the next word
+      } else {
+        // long string, exceeds the length
+        $chunks = str_split($word, $length);
+
+        // unload everything, except the very last chunk - it could be too short
+        for ($x = 0; $x < (count($chunks) - 1); $x++) {
+          if (rtrim($chunks[$x])) {
+            $result_arr []= rtrim($chunks[$x]);
+          }
+        }
+        $current_line = $chunks[count($chunks) - 1]; // let the next iteration of foreach figure it out
+      }
     }
   }
 
-  // Pass 3: combine the $words2 into lines of appropriate length
-  $words3 = array();
-  while (count($words2)) {
-    $words3 []= return_at_most($words2, $length);
-  } //end of while
+  if (rtrim($current_line)) { // we have something left over, and it's not space
+    $result_arr []= rtrim($current_line);
+  }
 
-  $result = implode("\n", $words3)."\n"; // trailing newline
+  return implode("\n", $result_arr);
+}
+
+
+function tokenize ($str) {
+  $result = array();
+  $result_index = 0;
+  $current_word = '';
+
+  for ($x = 0; $x < strlen($str); $x++) {
+    if (!$current_word) {
+      // we don't have anything, it's a brand new word
+      $current_word .= $str[$x];
+    } else {
+      $current_word_is_spaces = is_a_space($current_word[0]);
+      if (
+          ($current_word_is_spaces && is_a_space($str[$x])) ||
+          (!$current_word_is_spaces && !is_a_space($str[$x]))
+        ) {
+        // the easy part, append and move on
+        $current_word .= $str[$x];
+      } else {
+        // we either were collecting spaces and hit a word without them
+        // or vice versa
+        $result[] = $current_word;
+        $current_word = $str[$x];
+      }
+    }
+  }
+  // the for loop has ended, load whatever the remains onto the result
+  if ($current_word) {
+    $result[] = $current_word;
+    $current_word = '';
+  }
+
   return $result;
 }
 
-function return_at_most (&$string_arr, $max_length) {
-    $current = '';
-    $next = '';
+// the purpose of separating this into a function is to allow future modifications
+// to the definition of space without going around the code and changing
+// if() conditions elsewhere
 
-    $current = array_shift($string_arr);
-
-    while ($next = array_shift($string_arr)) {
-      if (strlen ($current . " " . $next) <= $max_length ) {
-        $current .= ' ' . $next;
-      } else {
-        // we're here because the length of the combined entity exceeds max_length
-        array_unshift($string_arr, $next);
-        return $current;
-      }
-    }
-
-    // we're here because there are no more elements left in $string_arr
-    return $current;
+function is_a_space ($c) {
+  return $c == ' ' || $c == '\t';
 }
+
 
 function wrap_testNormal($length) {
  print wrap("one two three four five six seven eight nine ten eleven twelve thirteen fourteeen fifteen", $length);
@@ -72,8 +102,8 @@ function wrap_testVeryLong() {
 }
 
 function wrap_testEmpty() {
-  print wrap('', 3);
-  print wrap('abcdef', 0);
+  print wrap('', 3); // output should be empty string
+  print wrap('abcdef', 0); // output should be the original string
 }
 
 function wrap_testBreakLongAndWrap() {
@@ -87,11 +117,11 @@ function wrap_testDuplets(){
 }
 
 
-wrap_testEmpty();
-wrap_testNormal(3);
-wrap_testNormal(5);
-wrap_testNormal(10);
-wrap_testNormal(15);
-wrap_testVeryLong();
-wrap_testBreakLongAndWrap();
-wrap_testDuplets();
+//wrap_testEmpty();
+//wrap_testNormal(3);
+//wrap_testNormal(5);
+// wrap_testNormal(10);
+// wrap_testNormal(15);
+// wrap_testVeryLong();
+//wrap_testBreakLongAndWrap();
+//wrap_testDuplets();
